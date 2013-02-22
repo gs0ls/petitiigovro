9fca68d17e00941e5ee74a3de45527cf8616aaf6
Calin Segarceanu <csegarceanu@gmail.com>
2013-02-22 22:29:29 +0200

    adaugat baza (codeigniter)

---
+++ application/controllers/prima_pagina.php
@@ -0,0 +1,23 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Prima_pagina extends CI_Controller {
+
+	/**
+		
+	*/
+	 
+	public function __construct(){
+	
+            parent::__construct();            
+			
+	}
+	
+	public function index(){
+	
+		$this->load->view('index');
+		
+	}
+}
+
+/* End of file index.php */
+/* Location: ./application/controllers/index.php */
---
+++ application/views/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+</head>
+<body>
+<?
+	echo '<h1>Prima pagina guvern</h1>';
+	echo ''
+?>
+
+</body>
+</html>
